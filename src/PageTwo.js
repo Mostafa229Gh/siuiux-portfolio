0c2ca53c8e7356a908bfefded98ebcc24b75e5bd
sinasalman <sina.salman381@gmail.com>
2024-09-28 15:20:38 +0330

    page2 now is ready to style

---
+++ src/PageTwo.js
@@ -0,0 +1,17 @@
+import './PageTwo.css';
+import circles from './asset/circles.png';
+
+export default function PageTwo(){
+    return(
+        <div className='container'>
+            <div className='Explore'>
+                <h1 className='header'>
+                    Explore my projects
+                </h1>
+                <></>
+                
+            </div>
+            
+        </div>
+    )
+}
