1b488700203a8579f68c9e0d754b6b32589f2450
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-09-29 17:55:57 +0330

    resolve a problem

--- src/PageOne.js
+++ src/PageOne.js
@@ -49,7 +49,7 @@ export default function PageOne() {
       </div>
 
       <h1>Designer</h1>
-      <div>
+      <div className="pageOneBottom">
         <svg
           width="100%"
           height="100"
