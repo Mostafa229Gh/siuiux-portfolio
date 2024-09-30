1c43c0d1ff52a49e49213f460ed51f30fab36ca7
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-09-30 17:48:03 +0330

    a little changes

--- src/App.js
+++ src/App.js
@@ -5,25 +5,38 @@ import PageOne from "./PageOne";
 import PageTwo from "./PageTwo";
 
 function App() {
-  
   const homeRef = useRef(null);
   const workRef = useRef(null);
-  const scrollToSection = (ref) => {
+  const aboutMeRef = useRef(null);
+  const resumeRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (ref, event) => {
     ref.current.scrollIntoView({ behavior: "smooth" });
+
+    document.querySelectorAll(".headerOption span").forEach((item) => {
+      item.style.fontWeight = "normal";
+      item.style.color = "#1e1e1e";
+    });
+
+    if (ref != homeRef) {
+      event.target.style.fontWeight = "bold";
+      event.target.style.color = "#fca311";
+    }
   };
 
   return (
     <div className="App">
       <header>
-        <div className="webName" onClick={() => scrollToSection(homeRef)}>
+        <div className="webName" onClick={(e) => scrollToSection(homeRef, e)}>
           <img id="logo" src={logo} alt="Logo" />
           <span>Sina Davari</span>
         </div>
         <div className="headerOption">
-          <span onClick={() => scrollToSection(workRef)}>Work</span>
-          <span>About me</span>
-          <span>Resume</span>
-          <span>Contact</span>
+          <span onClick={(e) => scrollToSection(workRef, e)}>Work</span>
+          <span onClick={(e) => scrollToSection(aboutMeRef, e)}>About me</span>
+          <span onClick={(e) => scrollToSection(resumeRef, e)}>Resume</span>
+          <span onClick={(e) => scrollToSection(contactRef, e)}>Contact</span>
         </div>
       </header>
 
@@ -34,7 +47,6 @@ function App() {
       <section ref={workRef}>
         <PageTwo />
       </section>
-
     </div>
   );
 }
