4d16da5169822d5b3dc35e140d1499699fe2616c
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-09-30 20:34:10 +0330

    add wheel feature for Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import logo from "./asset/siuinx.svg";
 import PageOne from "./PageOne";
 import PageTwo from "./PageTwo";
@@ -11,6 +11,8 @@ function App() {
   const aboutMeRef = useRef(null);
   const resumeRef = useRef(null);
   const contactRef = useRef(null);
+
+  const [activeSection, setActiveSection] = useState("home");
 
   const scrollToSection = (ref, event) => {
     ref.current.scrollIntoView({ behavior: "smooth" });
@@ -26,6 +28,62 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    const sections = [
+      { ref: homeRef, id: "home" },
+      { ref: workRef, id: "work" },
+      { ref: aboutMeRef, id: "aboutMe" },
+      { ref: resumeRef, id: "resume" },
+      { ref: contactRef, id: "contact" },
+    ];
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const section = sections.find(
+              (section) => section.ref.current === entry.target
+            );
+            if (section) {
+              setActiveSection(section.id);
+            }
+          }
+        });
+      },
+      { threshold: 0.6 } // Adjust as needed to detect when 60% of the section is visible
+    );
+
+    sections.forEach((section) => {
+      if (section.ref.current) {
+        observer.observe(section.ref.current);
+      }
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        if (section.ref.current) {
+          observer.unobserve(section.ref.current);
+        }
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    document.querySelectorAll(".headerOption span").forEach((item) => {
+      item.style.fontWeight = "normal";
+      item.style.color = "#1e1e1e";
+    });
+
+    const activeElement = document.querySelector(
+      `.headerOption span[data-section="${activeSection}"]`
+    );
+
+    if (activeElement) {
+      activeElement.style.fontWeight = "bold";
+      activeElement.style.color = "#fca311";
+    }
+  }, [activeSection]);
+
   return (
     <div className="App">
       <header>
@@ -34,10 +92,18 @@ function App() {
           <span>Sina Davari</span>
         </div>
         <div className="headerOption">
-          <span onClick={(e) => scrollToSection(workRef, e)}>Work</span>
-          <span onClick={(e) => scrollToSection(aboutMeRef, e)}>About me</span>
-          <span onClick={(e) => scrollToSection(resumeRef, e)}>Resume</span>
-          <span onClick={(e) => scrollToSection(contactRef, e)}>Contact</span>
+          <span data-section="work" onClick={(e) => scrollToSection(workRef, e)} >
+            Work
+          </span>
+          <span data-section="aboutMe" onClick={(e) => scrollToSection(aboutMeRef, e)} >
+            About me
+          </span>
+          <span data-section="resume" onClick={(e) => scrollToSection(resumeRef, e)} >
+            Resume
+          </span>
+          <span data-section="contact" onClick={(e) => scrollToSection(contactRef, e)} >
+            Contact
+          </span>
         </div>
       </header>
 
@@ -52,7 +118,7 @@ function App() {
       <section ref={aboutMeRef}>
         <PageThree />
       </section>
-
+      
     </div>
   );
 }
