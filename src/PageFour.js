ac469a4a09d3aab54fc9434603e47ee1492fdbb2
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-10-01 17:10:41 +0330

    start page four

--- src/PageFour.js
+++ src/PageFour.js
@@ -1,4 +1,21 @@
 import "./PageFour.css";
-export default function PageFour() {
+import React from "react";
+import deskImg from "./asset/contact.png"
 
+export default function PageFour() {
+    return (
+        <div className="containerFour">
+            <div className="contactMeText">Contact
+                <br/>Me</div>
+            <div className="contactMeSocials">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="deskImg">
+                <img id="deskImg" src={deskImg} />
+            </div>
+        </div>
+    )
 }
