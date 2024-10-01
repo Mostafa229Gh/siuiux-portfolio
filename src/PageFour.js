910b1abf0f74285c84d4520db94ad3bc6151e81e
Mostafa229Gh <Mostafa229Gh@gmail.com>
2024-10-01 18:00:25 +0330

    add contact me animation

--- src/PageFour.js
+++ src/PageFour.js
@@ -5,8 +5,10 @@ import deskImg from "./asset/contact.png"
 export default function PageFour() {
     return (
         <div className="containerFour">
-            <div className="contactMeText">Contact
-                <br/>Me</div>
+            <div className="contactMeText">
+                <p>Contact Me</p>
+                <div></div>
+            </div>
             <div className="contactMeSocials">
                 <div></div>
                 <div></div>
